e4785125819a0fbe96445189b3821403b8eb31d7
roman1rogov <rogovromansergeevich@yandex.ru>
2022-10-21 23:38:00 +0300

    add test

---
+++ FileManager/one_test1.py
@@ -0,0 +1,22 @@
+from func import func, f, math_pi, math_sqrt, math_pow, math_hypot
+
+assert func(3) == 3
+assert func(1) == 1
+
+assert f(2,4) == 6
+assert f(6,3) == 9
+assert f(1,1) == 2
+
+assert math_pi() == 3.141592653589793
+
+assert math_sqrt() == 4.47213595499958
+
+assert math_pow() == 8.0
+
+assert math_hypot() == 4.47213595499958
+
+
+
+
+
+
---
+++ FileManager/two_test.py
@@ -0,0 +1,4 @@
+from func import f
+assert f(2,4) == 6
+assert f(6,3) == 9
+assert f(1,1) == 2
